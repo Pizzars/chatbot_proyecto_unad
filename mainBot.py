2ef4a2a2c2aada2b567190fc86bd763e5f48e53d
Juan Jose Ruiz Soto <jyjdajj@gmail.com>
2022-11-13 19:49:39 -0500

    Terminando bot

--- mainBot.py
+++ mainBot.py
@@ -12,45 +12,52 @@ import pickle
 with open("contenido.json") as archivo:
     datos = json.load(archivo)
 
-palabras = []
-tags = []
-auxX = []
-auxY = []
+try:
+    with open("variables.pickle", "rb") as archivoPickle:
+        palabras, tags, entrenamiento, salida = pickle.load(archivoPickle)
+except: 
 
-for contenido in datos["contenido"]:
-    for patrones in contenido["patrones"]:
-        auxPalabra = nltk.word_tokenize(patrones)
-        palabras.extend(auxPalabra)
-        auxX.append(auxPalabra)
-        auxY.append(contenido["tag"])
+    palabras = []
+    tags = []
+    auxX = []
+    auxY = []
 
-        if contenido["tag"] not in tags:
-            tags.append(contenido["tag"]) 
+    for contenido in datos["contenido"]:
+        for patrones in contenido["patrones"]:
+            auxPalabra = nltk.word_tokenize(patrones)
+            palabras.extend(auxPalabra)
+            auxX.append(auxPalabra)
+            auxY.append(contenido["tag"])
 
-palabras = [stemmer.stem(w.lower()) for w in palabras if w!= "?"]
-palabras = sorted(list(set(palabras)))
-tags = sorted(tags)
+            if contenido["tag"] not in tags:
+                tags.append(contenido["tag"]) 
 
-entrenamiento = []
-salida = []
+    palabras = [stemmer.stem(w.lower()) for w in palabras if w!= "?"]
+    palabras = sorted(list(set(palabras)))
+    tags = sorted(tags)
 
-salidaVacia = [0 for _ in range(len(tags))]
+    entrenamiento = []
+    salida = []
 
-for x, documento in enumerate(auxX):
-    cubeta = []
-    auxPalabra = [stemmer.stem(w.lower()) for w in documento]
-    for w in palabras:
-        if w in auxPalabra:
-            cubeta.append(1)
-        else:
-            cubeta.append(0)
-    filaSalida = salidaVacia[:]
-    filaSalida[tags.index(auxY[x])] = 1
-    entrenamiento.append(cubeta)
-    salida.append(filaSalida)
+    salidaVacia = [0 for _ in range(len(tags))]
 
-entrenamiento = np.array(entrenamiento)
-salida = np.array(salida)
+    for x, documento in enumerate(auxX):
+        cubeta = []
+        auxPalabra = [stemmer.stem(w.lower()) for w in documento]
+        for w in palabras:
+            if w in auxPalabra:
+                cubeta.append(1)
+            else:
+                cubeta.append(0)
+        filaSalida = salidaVacia[:]
+        filaSalida[tags.index(auxY[x])] = 1
+        entrenamiento.append(cubeta)
+        salida.append(filaSalida)
+
+    entrenamiento = np.array(entrenamiento)
+    salida = np.array(salida)
+    with open("variables.pickle", "wb") as archivoPickle:
+        pickle.dump((palabras, tags, entrenamiento, salida), archivoPickle)
 
 ops.reset_default_graph()
 
@@ -61,6 +68,33 @@ red = tflearn.fully_connected(red, len(salida[0]), activation="softmax")
 red = tflearn.regression(red)
 
 modelo = tflearn.DNN(red)
-modelo.fit(entrenamiento, salida, n_epoch=1000, batch_size=10, show_metric=True)
-modelo.save("modelo.tflearn")
 
+try:
+    modelo.load("modelo.tflearn")
+except:
+    modelo.fit(entrenamiento, salida, n_epoch=1000, batch_size=10, show_metric=True)
+    modelo.save("modelo.tflearn")
+
+def mainBot():
+    seguir = True
+    while seguir:
+        entrada = input("Tu: ")
+        cubeta = [0 for _ in range(len(palabras))]
+        entradaProcesada = nltk.word_tokenize(entrada)
+        entradaProcesada = [stemmer.stem(palabra.lower()) for palabra in entradaProcesada]
+        for palabraIndividual in entradaProcesada:
+            for i, palabra in enumerate(palabras):
+                if palabra == palabraIndividual:
+                    cubeta[i] = 1
+        resultados = modelo.predict([np.array(cubeta)])
+        resultadosIndices = np.argmax(resultados)
+        tag = tags[resultadosIndices]
+
+        for tagAux in datos["contenido"]:
+            if tagAux["tag"] == tag:
+                seguir = tag != "despedida"
+                respuesta = tagAux["respuestas"]
+
+        print("BOT: ", random.choice(respuesta))
+
+mainBot()
